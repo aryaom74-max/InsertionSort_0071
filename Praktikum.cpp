d2c855051d52cf543809183cd643a7a49d9e2a32
aryaom74-max <aryaom74@gmail.com>
2026-03-12 19:42:25 +0700

    Fix formatting issues in Praktikum.cpp

--- Praktikum.cpp
+++ Praktikum.cpp
@@ -18,7 +18,7 @@ void input (){ //procedure input
     }
     cout << endl;
     cout << "=======================================";
-    cout << "==========Masukan Element000 Array==========";
+    cout << "==========Masukan Element Array==========";
     cout << "=======================================";
 
     for (int i = 0; i < n; i++ ){
@@ -66,6 +66,7 @@ int main(){
     display();
     system("pause");
 }
+
 
 
 
